6ac17508dcf503e505a79bf366b5760b2fd48316
Wibol <19147332+Wibol@users.noreply.github.com>
2022-03-13 10:47:55 +0100

    Update entrypoint.sh
    Set default client name (Dockerized > https://v.gd/cloudancer).
    Increased "client_buffer_size" to avoid buffer-overflow.
    Enabled only networks and protocols that still work (eDonkey and Kademlia).
    Set upload and download limits to infinite.
    Increased the number of upload slots.
    Decreased "ED2K-upload_timeout" to avoid upload queue full of inactive clients.
    Updated "web_infos" URLs.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,33 +4,34 @@ if [ ! -f /var/lib/mldonkey/downloads.ini ]; then
     mldonkey &
     echo "Waiting for mldonkey to start..."
     sleep 5
+    /usr/lib/mldonkey/mldonkey_command -p "" "set client_name Dockerized-https://v.gd/cloudancer" "save"
+    /usr/lib/mldonkey/mldonkey_command -p "" "set client_buffer_size 5000000" "save"
     /usr/lib/mldonkey/mldonkey_command -p "" "set run_as_user mldonkey" "save"
     /usr/lib/mldonkey/mldonkey_command -p "" "set allowed_ips 0.0.0.0/0" "save"
-    /usr/lib/mldonkey/mldonkey_command -p "" "share 0 /mnt/mldonkey_completed/files incoming_files" "save"
-    /usr/lib/mldonkey/mldonkey_command -p "" "share 0 /mnt/mldonkey_completed/directories incoming_directories" "save"
-    /usr/lib/mldonkey/mldonkey_command -p "" "unshare shared" "save"
-    /usr/lib/mldonkey/mldonkey_command -p "" "unshare incoming/files" "save"
-    /usr/lib/mldonkey/mldonkey_command -p "" "unshare incoming/directories" "save"
-    /usr/lib/mldonkey/mldonkey_command -p "" "set enable_overnet true" "save"
     /usr/lib/mldonkey/mldonkey_command -p "" "set enable_kademlia true" "save"
-    /usr/lib/mldonkey/mldonkey_command -p "" "set enable_bittorrent true" "save"
-    /usr/lib/mldonkey/mldonkey_command -p "" "set ED2K-connect_only_preferred_server true" "save"
-    /usr/lib/mldonkey/mldonkey_command -p "" "set max_hard_upload_rate 100" "save"
+    /usr/lib/mldonkey/mldonkey_command -p "" "set enable_bittorrent false" "save"
+    /usr/lib/mldonkey/mldonkey_command -p "" "set enable_overnet false" "save"
+    /usr/lib/mldonkey/mldonkey_command -p "" "set enable_directconnect false" "save"
+    /usr/lib/mldonkey/mldonkey_command -p "" "set enable_fileTP false" "save"
+    /usr/lib/mldonkey/mldonkey_command -p "" "set max_hard_upload_rate 0" "save"
     /usr/lib/mldonkey/mldonkey_command -p "" "set max_hard_download_rate 0" "save"
-    /usr/lib/mldonkey/mldonkey_command -p "" "set temp_directory /mnt/mldonkey_tmp" "save"
+    /usr/lib/mldonkey/mldonkey_command -p "" "set max_upload_slots 10" "save"
+    /usr/lib/mldonkey/mldonkey_command -p "" "set ED2K-connect_only_preferred_server false" "save"
     /usr/lib/mldonkey/mldonkey_command -p "" "set ED2K-max_connected_servers 4" "save"
+    /usr/lib/mldonkey/mldonkey_command -p "" "set ED2K-min_left_servers 5" "save"
     /usr/lib/mldonkey/mldonkey_command -p "" "set ED2K-firewalled-mode false" "save"
     /usr/lib/mldonkey/mldonkey_command -p "" "set ED2K-port 20562" "save"
+    /usr/lib/mldonkey/mldonkey_command -p "" "set ED2K-upload_timeout 60." "save"
     /usr/lib/mldonkey/mldonkey_command -p "" "set BT-dht_port 3617" "save"
     /usr/lib/mldonkey/mldonkey_command -p "" "set max_concurrent_downloads 150" "save"
     /usr/lib/mldonkey/mldonkey_command -p "" "set filenames_utf8 true" "save"
     /usr/lib/mldonkey/mldonkey_command -p "" "set create_file_mode 664" "save"
     /usr/lib/mldonkey/mldonkey_command -p "" "set create_dir_mode 775" "save"
     /usr/lib/mldonkey/mldonkey_command -p "" "set create_file_sparse true" "save"
-    /usr/lib/mldonkey/mldonkey_command -p "" "urladd server.met http://ed2k.2x4u.de/v1s4vbaf/min/server.met" "save"
+    /usr/lib/mldonkey/mldonkey_command -p "" "urladd server.met http://upd.emule-security.org/server.met" "save"
+    /usr/lib/mldonkey/mldonkey_command -p "" "urladd kad http://upd.emule-security.org/nodes.dat" "save"
     /usr/lib/mldonkey/mldonkey_command -p "" "urladd guarding.p2p http://upd.emule-security.org/ipfilter.zip" "save"
-    /usr/lib/mldonkey/mldonkey_command -p "" "urladd geoip.dat https://mirrors.download3k.com/token/a4f5830bcd49fec1856557a9204eacb7/GeoIP.dat.gz" "save"
-    /usr/lib/mldonkey/mldonkey_command -p "" "urladd hublist http://dchublist.com/hublist.config.bz2" "save"
+    /usr/lib/mldonkey/mldonkey_command -p "" "urladd geoip.dat http://mailfud.org/geoip-legacy/GeoIP.dat.gz" "save"
     if [ -z "$MLDONKEY_ADMIN_PASSWORD" ]; then
         /usr/lib/mldonkey/mldonkey_command -p "" "kill"
     else
@@ -44,7 +45,5 @@ if [ ! -f /var/lib/mldonkey/downloads.ini ]; then
 fi
 
 chown -R mldonkey:mldonkey /var/lib/mldonkey
-chown -R mldonkey:mldonkey /mnt/mldonkey_completed
-chown -R mldonkey:mldonkey /mnt/mldonkey_tmp
 
-mldonkey 
+mldonkey
